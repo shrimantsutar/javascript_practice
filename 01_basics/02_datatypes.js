caf644a322a75b0fc3dca88196dfe1d86f5c1953
Shrimant Sutar <136959327+shrimantsutar@users.noreply.github.com>
2025-06-18 19:35:28 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";       // treat all js code as newer version 
+
+// alert(2+3)          // we are using Nodejs, not Browser
+
+let Name= "shrimant"
+ let age =20
+ let isLogin = false
+
+
+
+ // number => 2 to power 53
+ // bigint 
+ // string => ""
+ // boolean => trut/false
+ // null => sandalone value
+ // undefined =>
+ // symble => unique
+
+
+//  object => 
+
+
+    console.log(typeof "shrimant");
+    console.log(typeof age);
+
+    console.log(typeof undefined);      //output=>    undefined
+    console.log(typeof null);           //output=>    object
+
+
+    
