6b427dfbd126f050f7956ee93f22134a597eca1f
Shrimant Sutar <136959327+shrimantsutar@users.noreply.github.com>
2025-06-18 19:37:10 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 133224
+let accountEmail = "aksjdh@gmail.com"
+var accountPass = "kjh23kj"
+accountCity = "Aurangabad"
+let accountState;
+
+// accountId=5         // Not Allwed     //Const Lihlya var value change kru shakat nahi 
+
+/*
+Never Used Var
+Bec of issue of block acope and fustional scope
+*/
+
+
+accountEmail="shrimant@gmail.com"
+accountPass ="aksuyi7"
+accountCity = "Pune"
+
+console.log(accountEmail);
+console.table([accountEmail,accountPass,accountCity,accountState]);
+
+
